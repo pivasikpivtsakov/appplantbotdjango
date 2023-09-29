b7f1f9bd6a8f00faaabe570d383a7e46882139a6
pivasikpivtsakov <elda250@live.com>
2023-09-29 23:54:35 +0600

    fix situation when no message in update

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -17,33 +17,34 @@ class TgWebhookView(APIView):
         update = telegram_api.models.Update.drf_serializer(data=request.data)
         if update.is_valid():
             data = update.validated_data
-            msg_text = data["message"]["text"].strip()
-            chat_id = data["message"]["chat"]["id"]
-            if msg_text == "/start":
-                msg_sending_result = send_to_tg_bot(
-                    "Добрый день! "
-                    "Пожалуйста, введите ваш логин, чтобы связать бота с сервисом и начать получать сообщения.",
-                    chat_id,
-                )
-                if msg_sending_result["ok"] is False:
-                    logger.warning(f"something bad when sending message to tg: {msg_sending_result} chat id: {chat_id}")
-                    return Response(status=400, data={"telegram_error_text": msg_sending_result["description"]})
-            else:
-                user = CustomUser.objects.get(telegram_id=chat_id)
-                if not user:
-
+            if data["message"]:
+                msg_text = data["message"]["text"].strip()
+                chat_id = data["message"]["chat"]["id"]
+                if msg_text == "/start":
                     msg_sending_result = send_to_tg_bot(
-                        "Логин установлен. Теперь введите токен",
+                        "Добрый день! "
+                        "Пожалуйста, введите ваш логин, чтобы связать бота с сервисом и начать получать сообщения.",
                         chat_id,
                     )
+                    if msg_sending_result["ok"] is False:
+                        logger.warning(f"something bad when sending message to tg: {msg_sending_result} chat id: {chat_id}")
+                        return Response(status=400, data={"telegram_error_text": msg_sending_result["description"]})
                 else:
-                    if user.user_token is None:
-                        user.telegram_id = chat_id
-                        user.save()
+                    user = CustomUser.objects.get(telegram_id=chat_id)
+                    if not user:
+
                         msg_sending_result = send_to_tg_bot(
-                            "Токен установлен. Бот успешно настроен на прием сообщений!",
-                            chat_id
+                            "Логин установлен. Теперь введите токен",
+                            chat_id,
                         )
+                    else:
+                        if user.user_token is None:
+                            user.telegram_id = chat_id
+                            user.save()
+                            msg_sending_result = send_to_tg_bot(
+                                "Токен установлен. Бот успешно настроен на прием сообщений!",
+                                chat_id
+                            )
         else:
             logger.warning("update incorrect")
         return Response(status=200)
