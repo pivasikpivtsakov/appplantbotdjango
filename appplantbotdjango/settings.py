9d1f8c4521aabb604560d2038fa02cfa96cbfa68
pivasikpivtsakov <elda250@live.com>
2023-09-30 06:09:39 +0600

    hide secret key

--- appplantbotdjango/settings.py
+++ appplantbotdjango/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q4jg951onuhpq_osta3-zl=m51_!phv#e^xp0=g2x8o*xcg4(6'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
